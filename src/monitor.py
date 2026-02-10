8d2e5f05e3c3900096229474257fa8ea413b8571
Hamid Sharifi <hamid-s1@hotmail.com>
2026-02-09 19:10:30 -0500

    added nvidia to inital render

--- src/monitor.py
+++ src/monitor.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTreeWidgetItem
 from PyQt6.QtCore import QSize
 from PyQt6 import uic
@@ -29,8 +30,83 @@ class MainWindow(QMainWindow):
         self.treeWidget.setIconSize(QSize(18, 18))
         self.resize(800,1000)
 
-        for i in self.components.hwmonx:
-            self.addHwmonEntry(i)
+        self.hasNvidia = self.detectNvidia()
+        if self.hasNvidia:
+            self.nvidia = nvidiaGPU.NvManager()
+            self.nvidia.refresh()
+            for gpu in self.nvidia.gpus.values():
+                self.addNvidiaEntry(gpu)
+
+        for hwmon in self.components.hwmonx:
+            self.addHwmonEntry(hwmon)
+
+    def detectNvidia(self) -> bool:
+        try:
+            subprocess.check_output(["nvidia-smi"], stderr = subprocess.DEVNULL)
+            return True
+        except Exception:
+            return False
+
+    def addNvidiaEntry(self, gpu: nvidiaGPU.NvGPU):
+        entry = QTreeWidgetItem([f"{gpu.model} (GPU {gpu.id})"])
+        self.treeWidget.addTopLevelItem(entry)
+        
+        entryType = QTreeWidgetItem(["Temperature"])
+        entryType.setIcon(0, QIcon('../assets/icons/Temperature.svg'))
+        entry.addChild(entryType)
+        unit = "°C" if self.isCelcius else "°F" 
+
+        dataRow = QTreeWidgetItem([
+            "Temperature",
+            f"{self.convertTemp(gpu.temp.currentValue, self.isCelcius)} {unit}",
+            f"{self.convertTemp(gpu.temp.minValue, self.isCelcius)} {unit}",
+            f"{self.convertTemp(gpu.temp.maxValue, self.isCelcius)} {unit}"
+        ])
+
+        entryType.addChild(dataRow)
+        self.nvRows[f"GPU{gpu.id}_temp"] = (dataRow, gpu)
+        entryType.setExpanded(True)
+
+        entryType = QTreeWidgetItem(["Power"])
+        entryType.setIcon(0, QIcon('../assets/icons/Power.svg'))
+        entry.addChild(entryType)
+
+        dataRow = QTreeWidgetItem([
+            "Power",
+            f"{gpu.power.currentValue} W",
+            f"{gpu.power.minValue} W",
+            f"{gpu.power.maxValue} W"
+        ])
+
+        entryType.addChild(dataRow)
+        self.nvRows[f"GPU{gpu.id}_pwr"] = (dataRow, gpu)
+        entryType.setExpanded(True)
+
+        clockEntry = QTreeWidgetItem(["Clock"])
+        clockEntry.setIcon(0, QIcon('../assets/icons/Clock.svg'))
+        entry.addChild(clockEntry)
+        
+        gcRow = QTreeWidgetItem([
+            "Graphics",
+            f"{gpu.graphicsClock.currentValue} MHz",
+            f"{gpu.graphicsClock.minValue} MHz",
+            f"{gpu.graphicsClock.maxValue} MHz"
+        ])
+
+        mcRow = QTreeWidgetItem([
+            "Memory",
+            f"{gpu.memoryClock.currentValue} MHz",
+            f"{gpu.memoryClock.minValue} MHz",
+            f"{gpu.memoryClock.maxValue} MHz"
+        ])
+
+        clockEntry.addChild(gcRow)
+        clockEntry.addChild(mcRow)
+        self.nvRows[f"GPU{gpu.id}_gc"] = (gcRow, gpu)
+        self.nvRows[f"GPU{gpu.id}_mc"] = (mcRow, gpu)
+        
+        clockEntry.setExpanded(True)
+        entry.setExpanded(True)
 
     def addHwmonEntry(self, hwmon: sensors.HwmonDevice):
         device = QTreeWidgetItem([hwmon.name])
@@ -44,14 +120,13 @@ class MainWindow(QMainWindow):
                 entryType.setExpanded(True)
                 sensType = sensor.sensType
                 entryType = QTreeWidgetItem([sensType])
-                entryType.setIcon(0, QIcon(f'../icons/{sensType}.png'))
+                entryType.setIcon(0, QIcon(f'../assets/icons/{sensType}.svg'))
                 device.addChild(entryType)
 
             dataRow = self.createSensorRow(sensor)
 
             unique_id = f"{hwmon.id}_{sensor.label}"
             self.sensorRows[unique_id] = (dataRow, sensor)
-
 
             entryType.addChild(dataRow)
 
@@ -106,7 +181,6 @@ class MainWindow(QMainWindow):
     def convertTemp(self, temp: float, isCelcius: bool) -> float:
         if isCelcius:
             return temp
-        
         return round((temp * 9/5) + 32, 2)
 
 if __name__ == '__main__':
